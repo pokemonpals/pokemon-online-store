74894eafe5909c6285d5d6d77d7a45d60247bf33
Constance Kang <linweikang1203@gmail.com>
2019-06-12 19:57:53 -0400

    update nav bar and change the index website title

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,31 +4,61 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>Pokemon Store</h1>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/products">Products</Link>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+//material ui:
+import {makeStyles} from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper
+  }
+}))
+
+export function Navbar({handleClick, isLoggedIn}) {
+  const classes = useStyles()
+  const [value, setValue] = React.useState(0)
+
+  function handleChange(event, newValue) {
+    setValue(newValue)
+  }
+
+  return (
+    <div className>
+      <AppBar position="static">
+        <Tabs variant="fullWidth" value={value} onChange={handleChange}>
+          <Tab label="Home" component={Link} to="/" />
+          <Tab label="Products" component={Link} to="/products" />
+          <Tab label="Cart" component={Link} to="/cart">
+            <img id="bag" src="https://img.icons8.com/color/420/pokebag.png" />
+          </Tab>/>
+          {isLoggedIn ? (
+            <div>
+              <Tab
+                component={Link}
+                to="/me"
+                label="                My Account"
+              />
+              <Tab
+                label="Logout"
+                onClick={handleClick}
+                component={Link}
+                to="#"
+              />
+            </div>
+          ) : (
+            <div>
+              <Tab label="Login" component={Link} to="/login" />
+              <Tab label="Sign Up" component={Link} to="/signup" />
+            </div>
+          )}
+        </Tabs>
+      </AppBar>
+    </div>
+  )
+}
 
 /**
  * CONTAINER
