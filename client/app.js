74dee92742f8b41f6bec4b4da23c2750449ff40f
Constance Kang <linweikang1203@gmail.com>
2019-06-14 17:42:34 -0400

    comment out user-home.spec.js needs to make a new test spec for the User Home page

--- client/app.js
+++ client/app.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Navbar} from './components'
 import Routes from './routes'
 //material ui:
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Routes />
+      </div>
+    )
+  }
 }
 
 export default App
