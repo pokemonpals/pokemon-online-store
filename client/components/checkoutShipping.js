90b827f877585a512b9118d6b09a67f3a15127a8
Constance Kang <linweikang1203@gmail.com>
2019-06-15 18:41:23 -0400

    clean up to the UpdateUserProfile

--- client/components/checkoutShipping.js
+++ client/components/checkoutShipping.js
@@ -8,6 +8,7 @@ import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Checkbox from '@material-ui/core/Checkbox'
+import Button from '@material-ui/core'
 
 class CheckoutShipping extends Component {
   constructor(props) {
@@ -28,6 +29,7 @@ class CheckoutShipping extends Component {
     this.setState({
       [event.target.name]: event.target.value
     })
+    this.props.updateUser(this.props.user.id, this.state)
   }
 
   handleSubmit(event) {
@@ -36,79 +38,94 @@ class CheckoutShipping extends Component {
   }
 
   render() {
+    console.log('this.props', this.props)
     return (
       <React.Fragment>
         <Typography variant="h6" gutterBottom>
           Shipping Address
         </Typography>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              id="firstName"
-              name="firstName"
-              label="First name"
-              fullWidth
-              autoComplete="fname"
-            />
+        <form onSubmit={this.props.onSubmit}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="firstName"
+                name="firstName"
+                value={this.props.user.firstName}
+                onChange={this.props.handleChange}
+                label="First name"
+                fullWidth
+                autoComplete="fname"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="lastName"
+                name="lastName"
+                value={this.props.user.lastName}
+                label="Last name"
+                fullWidth
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="address"
+                name="address"
+                value={this.props.user.address}
+                label="Address"
+                fullWidth
+                autoComplete="billing address-line1"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="city"
+                name="city"
+                value={this.props.user.city}
+                label="City"
+                fullWidth
+                autoComplete="billing address-level2"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="state"
+                name="state"
+                value={this.props.user.state}
+                label="State/Province/Region"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="zipcode"
+                name="zipcode"
+                value={this.props.user.zipcode}
+                label="Zip / Postal code"
+                fullWidth
+                autoComplete="billing postal-code"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    color="secondary"
+                    name="saveAddress"
+                    value="yes"
+                    type="submit"
+                  />
+                }
+                label="Use this address for payment details"
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              id="lastName"
-              name="lastName"
-              label="Last name"
-              fullWidth
-              autoComplete="lname"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              required
-              id="address"
-              name="address"
-              label="Address"
-              fullWidth
-              autoComplete="billing address-line1"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              id="city"
-              name="city"
-              label="City"
-              fullWidth
-              autoComplete="billing address-level2"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="state"
-              name="state"
-              label="State/Province/Region"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              id="zipcode"
-              name="zipcode"
-              label="Zip / Postal code"
-              fullWidth
-              autoComplete="billing postal-code"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormControlLabel
-              control={
-                <Checkbox color="secondary" name="saveAddress" value="yes" />
-              }
-              label="Use this address for payment details"
-            />
-          </Grid>
-        </Grid>
+        </form>
       </React.Fragment>
     )
   }
