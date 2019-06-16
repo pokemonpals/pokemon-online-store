690bb94312cffcf8772f5024681b70f26df1a527
Jennifer Scheinhorn <jscheinhorn@gmail.com>
2019-06-16 00:14:56 -0400

    refactor: updated merges

--- client/components/Home.js
+++ client/components/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1>ONLY LEGAL AUTHORIZED FREE-RANGE POKEMON 'DEALER' ON THE WEB</h1>
+      Random internet things:
       <img src="https://www.mypokecard.com/my/galery/u2C7zRVu0JAG.jpg" />
       <img src="https://static.fjcdn.com/large/pictures/ac/a7/aca7ec_1781618.jpg" />
     </div>
